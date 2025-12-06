c95ce546154e02387cc885cba857f9c59d719870
Hilal Mufti <hmufti@cs.washington.edu>
2025-12-06 15:16:26 -0800

    Implemented SGD

--- main.py
+++ main.py
@@ -1,88 +1,147 @@
-from typing import NamedTuple
+import math
+from functools import partial
+from typing import TypedDict 
 
-import mlx as mx
+import mlx.core as mx
+from mlx.utils import tree_map
 import numpy as np
 import numpy.typing as npt
 
+
+# -------------------------------- type alias ----------------------------------
+
+Array = npt.NDArray[np.float32]
+
+# ---------------------------------- datasets ----------------------------------
+
+def load_bixor(nsamples=32):
+    ...
+
+# TODO: Return a dict here?
+def load_donut(ntrain=32, ntest=8, r=0.5):
+    X_train = np.random.uniform(-1, 1, size=(ntrain, 2))
+    y_train = np.array([1. if math.sqrt(x[0]**2 + x[1]**2) < r else -1. for x in X_train.tolist()])
+    X_test = np.random.uniform(-1, 1, size=(ntest, 2))
+    y_test = np.array([1. if math.sqrt(x[0]**2 + x[1]**2) < r else -1. for x in X_test.tolist()])
+    return X_train, y_train, X_test, y_test
+
+def load_sincos(nsamples=32):
+    ...
+
+# --------------------------------- optimizers ---------------------------------
+
+class MySGD(TypedDict):
+    lr: float
+
+def sgdnew(lr: float) -> MySGD:
+    return MySGD(lr=lr)
+
+# TODO: Should I make this private?
+def sgdupdate1(sgd, param, grad):
+    lr = sgd['lr']
+    return param - lr * grad
+
+def sgdupdate(sgd, params, grads):
+    return tree_map(lambda param, grad: sgdupdate1(sgd, param, grad), params, grads) 
+
+# TODO
+class MyAdam(TypedDict):
+    ...
+
+# TODO 
+def adamnew():
+    ...
+
+# TODO
+def adamupdate():
+    ...
+
 # ---------------------------------- models ------------------------------------
 
-class RNNCell(NamedTuple):
-    wx: npt.NDArray[np.float32]
-    wh: npt.NDArray[np.float32]
+# TODO: Support bias
+class RNNCell(TypedDict):
+    wx: Array 
+    wh: Array 
 
-def rnncellnew(input_dim, hidden_dim):
+def rnncellnew(input_dim, hidden_dim) -> RNNCell:
     wx = np.random.normal(size=(input_dim, hidden_dim)) # TODO: Use better init
     wh = np.random.normal(size=(hidden_dim, hidden_dim))
-    return RNNCell(wx, wh)
+    return RNNCell(wx=wx, wh=wh)
 
 # TODO: Play with this activation
 # Note: If any of the entries is large, tanh makes it go to 1
-def rnncellfwd(cell, xt, prevh):
-    (wx, wh) = cell
-    ht = np.tanh(np.matmul(xt, wx) + np.matmul(prevh, wh)) # TODO: Why backwards?
+def rnncellfwd(cell, xt, hprev):
+    wx, wh = cell['wx'], cell['wh']
+    ht = np.tanh(np.matmul(xt, wx) + np.matmul(hprev, wh)) # TODO: Why are the matmuls transposed?
     return ht
 
-class RNN(NamedTuple):
-    Wx: npt.NDArray[np.float32]
-    Wh: npt.NDArray[np.float32]
-    b:  npt.NDArray[np.float32]
+# TODO
+def rnncellbwd(cell, dht, cache):
+    wx, wh = cell['wx'], cell['wh']
+    (xt, prevh, ht) = cache
+    ...
 
-def rnnfwd(rnn: RNN):
+class RNN(TypedDict):
+    cell: RNNCell
+
+def rnnnew(input_dim, hidden_dim):
+    cell = rnncellnew(input_dim, hidden_dim)
+    return RNN(cell)
+
+# TODO
+def rnnfwd(rnn: RNN, xs, h0):
+    y, h = ...
+    return y, h
+
+# TODO
+class MyDataset(TypedDict):
     ...
 
 # TODO
-class MyDataset(NamedTuple):
+class MyDataLoader(TypedDict):
     ...
 
 # TODO
-class MyDataLoader(NamedTuple):
+class MyLRScheduler(TypedDict):
     ...
 
 # TODO
-class MyAdam(NamedTuple):
+class RNNEncoder(TypedDict):
     ...
 
 # TODO
-class MyLRScheduler(NamedTuple):
+class RNNDecoder(TypedDict):
     ...
 
 # TODO
-class RNNEncoder(NamedTuple):
+class RNNPointerNet(TypedDict):
     ...
 
 # TODO
-class RNNDecoder(NamedTuple):
-    ...
-
-# TODO
-class RNNPointerNet(NamedTuple):
-    ...
-
-# TODO
-class MyNCO(NamedTuple):
+class MyNCO(TypedDict):
     embedding: npt.NDArray[np.float32]
     actor_net: RNNPointerNet
 
 # TODO: I don't think I need Attention, PointerNet for just using RNN's?
 
 # TODO
-class Encoder(NamedTuple):
+class Encoder(TypedDict):
     ...
 
 # TODO
-class Attention(NamedTuple):
+class Attention(TypedDict):
     ...
 
 # TODO
-class Decoder(NamedTuple):
+class Decoder(TypedDict):
     ...
 
 # TODO
-class PointerNet(NamedTuple):
+class PointerNet(TypedDict):
     ...
 
 # TODO
-class NCO(NamedTuple):
+class NCO(TypedDict):
     ...
 
 # -----------
@@ -111,6 +170,5 @@ if __name__ == "__main__":
 # - [ ] Implement or find OR-Tools baseline
 # - [ ] Implement or find optimal baseline (use Concorde)
 # - [ ] How does the Python object system work?
-# - [ ] How does NamedTuple work?
 # - [ ] Try generating data from a different distribution than U([0, 1]^2)
 # - [ ] Fun: pure C implementation
--- test.py
+++ test.py
@@ -1,15 +1,69 @@
 import sys
 
-import mlx as mx
+import mlx.core as mx
 import numpy as np
 
 from main import *
+
+
+def sgdupdate_basic():
+    sgd = sgdnew(lr=1e-1) # lr=0.1
+
+    params = {
+        'w0': np.array([1., 2.]),
+        'w1': np.array([3., 4.]),
+        'b': np.array([5.]) 
+    }
+    grads = {
+        'w0': np.array([5., 16.]) ,
+        'w1': np.array([28., 39.]),
+        'b': np.array([47.]) 
+    }
+    params = sgdupdate(sgd, params, grads)
+    assert np.allclose(params['w0'], np.array([0.5, 0.4]))
+    assert np.allclose(params['w1'], np.array([0.2, 0.1]))
+    assert np.allclose(params['b'], np.array([0.3]))
+
+def sgdupdate_nested():
+    sgd = sgdnew(lr=1e-1) # lr=0.1
+
+    params = {
+        'w0': {
+            'w00': np.array([1., 2.]),
+            'w01': np.array([2., 1.]),
+            'b0': {
+                'b00': np.array([6.]),
+                'b01': np.array([7.])
+            }
+        },
+        'w1': np.array([3., 4.]),
+        'b': np.array([5.]) 
+    }
+    grads = {
+        'w0': {
+            'w00': np.array([9., 19.5]),
+            'w01': np.array([18., 9.]),
+            'b0': {
+                'b00': np.array([50.5]),
+                'b01': np.array([55.])
+            }
+        },
+        'w1': np.array([28., 39.]),
+        'b': np.array([47.]) 
+    }
+    params = sgdupdate(sgd, params, grads)
+    assert np.allclose(params['w0']['w00'], np.array([0.1, 0.05]))
+    assert np.allclose(params['w0']['w01'], np.array([0.2, 0.1]))
+    assert np.allclose(params['w0']['b0']['b00'], np.array([0.95]))
+    assert np.allclose(params['w0']['b0']['b01'], np.array([1.5]))
+    assert np.allclose(params['w1'], np.array([0.2, 0.1]))
+    assert np.allclose(params['b'], np.array([0.3]))
 
 def rnncellfwd_basic():
     np.random.seed(546)
 
     cell = rnncellnew(2, 3)
-    (wx, wh) = cell
+    wx, wh = cell['wx'], cell['wh']
     assert np.allclose(wx, np.array([[-0.06553868,  0.40492537,  1.05272082],
                                      [-0.01006521,  2.69813824, -0.00936148]]))
     assert np.allclose(wh, np.array([[-0.03541319,  0.97702002, -0.14594773],
@@ -19,16 +73,20 @@ def rnncellfwd_basic():
     hprev = np.array([3., 4., 5.])
 
     ht = rnncellfwd(cell, xt, hprev)
-    assert np.allclose(ht, np.array([ 0.38528493,  1., -0.30972828]))
+    assert np.allclose(ht, np.array([0.38528493,  1., -0.30972828]))
 
+# use seq of length 3 and h0 = [0, 0, 1]
 def rnnfwd_basic():
     assert False
 
 TESTS = [
+    ("sgdupdate_basic", sgdupdate_basic),
+    ("sgdupdate_nested", sgdupdate_nested),
     ("rnncell_basic", rnncellfwd_basic),
     ("rnnfwd_basic", rnnfwd_basic)
 ]
 
+# TODO: Add color to the tests
 def main():
     npass = 0
     for testname, test in TESTS:
@@ -37,7 +95,7 @@ def main():
             npass += 1
             print(f"{testname} - pass")
         except AssertionError as e:
-            print(f"{testname} - error")
+            print(f"{testname} - fail!!!")
     print(f"{npass}/{len(TESTS)} tests pass")
 
     
